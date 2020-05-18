bc2344d02286f7f4c0eea29aabb3192d2015a839
RAYZP <532415316@qq.com>
2020-05-18 22:08:40 +0800

    完成将天气显示到界面的功能。

---
+++ app/src/main/java/com/example/coolweather/gson/Forecast.java
@@ -0,0 +1,26 @@
+package com.example.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Forecast {
+    @SerializedName("date")
+    public String date;
+
+    @SerializedName("tmp")
+    public Temperature temperature;
+
+    public class Temperature{
+        @SerializedName("max")
+        public String max;
+        @SerializedName("min")
+        public String min;
+    }
+
+    @SerializedName("cond")
+    public More more;
+
+    public class More{
+        @SerializedName("txt_d")
+        public String info;
+    }
+}
